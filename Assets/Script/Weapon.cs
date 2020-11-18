3d58c191f963c70915dff63633fbddd8ff2218ee
Elegalaxy <yokai.czk@gmail.com>
2020-11-19 00:03:27 +1030

    19 Nov 2020
    - Upload image
    - Done bullet

---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bullet;
+    int weaponIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+        }
+    }
+}
